9a8fde8100845bc2c9e99e6ec9a31eb9db36bd4c
Herman.He <mocaphgf@foxmail.com>
2016-07-21 15:13:44 +0800

    Create 随机选文件夹部分图片.py

---
+++ 随机选文件夹部分图片.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# 2016-07-21
+
+'''
+random selecting some images
+'''
+import os
+import re
+import shutil
+import random
+
+if __name__ == '__main__':
+
+    # Read the json data from text file
+    f = open('image.txt','r')
+    # print f.read()
+    datas = f.readlines()
+    print len(datas)
+    #print datas[1]
+
+    slices = random.sample(datas, 200)
+    #print slices
+    for data in slices:
+        datasplit = re.split('.jpg', data)
+        fileName = datasplit[0]
+        fileName = fileName+'.jpg'
+        print 'Processing the image: ', fileName
+        
+        shutil.move(fileName,"./test/")    
